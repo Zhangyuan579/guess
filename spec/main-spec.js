c49f981ed8ee871a556abb2abc78c40dc13c0a58
Zhangyuan579 <1522687541@qq.com>
2018-05-31 21:31:15 +0800

    first commit

---
+++ spec/main-spec.js
@@ -0,0 +1,17 @@
+const main = require('../main/main');
+
+describe('main()', () => {
+    describe("The 'toEqual' matcher", function() {
+        it("works for simple literals and variables", function() {
+            let input = '1234';
+            let a = main(input);
+            expect(a).toEqual('4A0B');
+        });
+        it("works for simple literals and variables", function() {
+            let input = '2222';
+            let a = main(input);
+            expect(a).toEqual('1A3B');
+        });
+    });
+
+});
